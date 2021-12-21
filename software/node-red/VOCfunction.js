a79945138d61cfaafc7ccd2c64073e848cc61011
Noel Logan 18003 <75576766+Tateuf@users.noreply.github.com>
2021-12-21 12:40:24 +0100

    Create VOCfunction.js

---
+++ software/node-red/VOCfunction.js
@@ -0,0 +1,17 @@
+var vo = flow.get("VOC");
+var lat = flow.get("latitude");
+var lon = flow.get("longitude");
+var data = {
+    "Country": "Belgium",
+    "Function": "1-3-----",
+    "LOCODE": "BEBRU",
+    "Name": "Bruxelles",
+    "NameWoDiac": "Bruxelles",
+    "Status": "Bruxelles",
+    "outflows": JSON.stringify(vo),
+    "lat": JSON.stringify(lat),
+    "long":  JSON.stringify(lon)
+    }
+var newpay = msg.payload.substring(0, msg.payload.length - 1);
+msg.payload = newpay + "," +JSON.stringify(data)+"]";
+return msg;
